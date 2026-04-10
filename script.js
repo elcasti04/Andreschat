6c083838d49bbf99d50d0050eab2bd485c062dcb
AndresCastro <188876553+elcasti04@users.noreply.github.com>
2026-04-09 20:43:58 -0500

    Agregando nuevas secciones

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ async function enviar() {
 async function obtenerRespuesta(mensaje) {
 	try {
 		const res = await fetch(
-			'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=process.env.API_KEY',
+			'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=API_KEY',
 			{
 				method: 'POST',
 				headers: {
